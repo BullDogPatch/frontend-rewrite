3307736165de4848444bf7a0c77efbc8daa86a3d
Craig Clayton <craig__clayton@outlook.com>
2024-02-21 16:58:06 +0000

    add images to page conditionally depending on topic

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,9 +1,22 @@
+import footballImage from "../images/football.png";
+import cookingImage from "../images/cooking.png";
+import codingImage from "../images/coding.png";
+
 const ArticleCard = ({ article }) => {
   return (
     <li>
       <h3>{article.title}</h3>
       <p>{article.topic}</p>
-      {/* <div>{article}</div> */}
+      <div>
+        {" "}
+        {article.topic === "coding" ? (
+          <img className="topic-image" src={codingImage} alt="coding" />
+        ) : article.topic === "cooking" ? (
+          <img className="topic-image" src={cookingImage} alt="cooking" />
+        ) : (
+          <img className="topic-image" src={footballImage} alt="football" />
+        )}
+      </div>
     </li>
   );
 };
