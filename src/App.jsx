60edf4c7a042ab68425eca4f6761180c9725bce1
Craig Clayton <craig__clayton@outlook.com>
2024-02-21 16:21:53 +0000

    add few files for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <div className="container">
+      <Header />
       <h1>Hello, world</h1>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/ArticleCard.jsx
@@ -0,0 +1,5 @@
+const ArticleCard = () => {
+  return <h1>Atricle card</h1>;
+};
+
+export default ArticleCard;
