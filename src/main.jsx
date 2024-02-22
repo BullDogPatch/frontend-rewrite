0a1041bc9f265bcbe66f69d4312b4b9e090c3941
Craig Clayton <craig__clayton@outlook.com>
2024-02-22 18:31:13 +0000

    add roots folder, will create another branch to try react-router v6 new api

--- src/main.jsx
+++ src/main.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {
   BrowserRouter,
-  createBrowserRouter,
-  RouterProvider,
+  // createBrowserRouter,
+  // RouterProvider,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
+// import Index from "./roots/index.js";
 
-const router = createBrowserRouter([
-  {
-    // path='/',
-    path: "*",
-    element: <App />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     // path='/',
+//     path: "*",
+//     element: <App />,
+//     children: [{ index: true, element: <Index /> }],
+//   },
+// ]);
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <App />
-      </BrowserRouter> */}
-      <RouterProvider router={router} />
+      </BrowserRouter>
+      {/* <RouterProvider router={router} /> */}
     </QueryClientProvider>
   </React.StrictMode>
 );
