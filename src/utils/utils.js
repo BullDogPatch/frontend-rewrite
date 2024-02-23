5e054e504fd1724826deaeef6a756081bdf8fad2
Craig Clayton <craig__clayton@outlook.com>
2024-02-23 20:00:23 +0000

    write fetchCommentsByArticleId function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,6 +14,11 @@ export const fetchArticleById = async (article_id) => {
   return data;
 };
 
+export const fetchCommentsByArticleId = async (article_id) => {
+  const { data } = await api.get(`/articles/${article_id}/comments`);
+  return data.comments;
+};
+
 export const fetchAllUsers = async () => {
   const { data } = await api.get("/users");
   return data.users;
